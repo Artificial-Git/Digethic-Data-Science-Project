e1d66290e29063a40063b18df455217c1f19b752
Artificial-Git <99414315+Artificial-Git@users.noreply.github.com>
2022-05-06 00:40:32 +0200

    Added script testimg imputation of missing values and one for explorative data analysis

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
-python3 -m venv .venv
+python -m venv .venv
 .\.venv\Scripts\Activate.ps1
 pip install --upgrade pip
+#python -m pip install -U --force-reinstall pip setuptools wheel
 pip install -r requirements.txt
