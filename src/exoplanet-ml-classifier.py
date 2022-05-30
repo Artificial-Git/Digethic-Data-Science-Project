2c4170715d8bde96d2b729c6305fbddad906cdab
Artificial-Git <99414315+Artificial-Git@users.noreply.github.com>
2022-05-30 20:18:13 +0200

    Erster Durchlauf des Scripts

--- src/exoplanet-ml-classifier.py
+++ src/exoplanet-ml-classifier.py
@@ -18,19 +18,23 @@ import shap
 from statsmodels import robust
 import time
 
-data = pd.read_csv('../data/cumulative.csv')
+data = pd.read_csv('data/cumulative_2022.04.28_05.30.33.csv', header=53))
 
+print("Kepler-Object-of-Interest-Datensatz wurde eingelesen.")
 
 # Entfernung von übeflüssigen Spalten ohne Informationsgehalt:
 # Spalten mit "id" oder "name" im Namen enthalten nur Bezeichnungen. Die beiden "koi_teq_err"-
 # Spalten sind leer.
 
-to_pop = ["rowid", "kepid", "kepoi_name", "kepler_name",
+to_pop=["rowid", "kepid", "kepoi_name", "kepler_name",
           "koi_teq_err1", "koi_teq_err2", "koi_tce_delivname"]
 
 for col in to_pop:
 
     data.pop(col)
+
+print("Irrelevante Features wurden entfernt.")
+
 
 # Umwandelung von Ordinalwerten in numerische Werte:
 
@@ -41,8 +45,12 @@ koi_pdisposition_dict = {'FALSE POSITIVE': 0, 'CANDIDATE': 1}
 data["koi_pdisposition"] = data["koi_pdisposition"].replace(
     koi_pdisposition_dict)
 
+print("Ordinalwerten wurden in numerische Werte umgewandelt.")
+
 
 # Ersetzung fehlender Werte durch imputierte Werte
+
+print("Ersetzung fehlender Werte durch imputierte Werte.")
 
 knn_imputer = KNNImputer()
 
@@ -52,25 +60,33 @@ for column in data:
         data[column].to_numpy().reshape(-1, 1))
 
 
+print("Fehlende Werte wurden mit kNN-Imputer ersetzt.")
+
 # Korrelationsmatrix erstellen
 
-fig = plt.figure(figsize=(14.14, 14.14), dpi=300)
+print("Korrelationsmatrix erstellen.")
+
+fig = plt.figure(figsize=(14.14, 11.14), dpi=300)
 plt.title("Correlation heatmap of Kepler Objects of Interest dataset features")
 sns.set(font_scale=1)
 sns.heatmap(data.corr(), annot=True, annot_kws={"fontsize": 6}, fmt='.2f')
 
-plt.savefig('../data/figures/heatmap.png')
+plt.savefig('data/figures/heatmap.png')
+
+print("Korrelationsmatrix wurde erstellt.")
 
 
 # Streudiagrammsmatrix erstellen
+
+print("Streudiagrammsmatrix erstellen.")
 
 scatter_data = data.copy()
 to_pop = ['koi_period_err1', 'koi_period_err2', 'koi_time0bk', 'koi_time0bk_err1',
           'koi_time0bk_err2', 'koi_impact', 'koi_impact_err1', 'koi_impact_err2',
           'koi_duration_err1', 'koi_duration_err2', 'koi_depth', 'koi_depth_err1', 'koi_depth_err2',
-           'koi_prad_err1', 'koi_prad_err2', 'koi_insol', 'koi_insol_err1', 'koi_insol_err2',
-           'koi_steff_err2', 'koi_slogg', 'koi_slogg_err1', 'koi_slogg_err2', 'koi_srad',
-           'koi_srad_err1', 'koi_srad_err2', 'ra', 'dec', 'koi_kepmag']
+          'koi_prad_err1', 'koi_prad_err2', 'koi_insol', 'koi_insol_err1', 'koi_insol_err2',
+          'koi_steff_err2', 'koi_slogg', 'koi_slogg_err1', 'koi_slogg_err2', 'koi_srad',
+          'koi_srad_err1', 'koi_srad_err2', 'ra', 'dec', 'koi_kepmag']
 
 for col in to_pop:
 
@@ -87,11 +103,15 @@ for ax in scatter_matrix.ravel():
 plt.suptitle(
     "Scatter Matrix of selected Kepler Objects of Interest dataset features", y=1)
 plt.tight_layout(pad=0.2)
-plt.savefig('../data/figures/scatter_matrix.png')
+plt.savefig('data/figures/scatter_matrix.png')
+
+print("Streudiagrammsmatrix wurde erstellt.")
 
 
 # Kastengrafik erstellen
 # Werte skalieren um die Kastengrafik darstellen zu können.
+
+print("Kastengrafik erstellen.")
 
 scaled_data = data.copy()
 minmax = MinMaxScaler()
@@ -99,7 +119,7 @@ minmax = MinMaxScaler()
 for column in scaled_data:
 
     scaled_data[column] = minmax.fit_transform(
-    scaled_data[column].to_numpy().reshape(-1, 1))
+        scaled_data[column].to_numpy().reshape(-1, 1))
 
 fig = plt.figure(figsize=(15, 7.5))
 plt.title("Boxplot of the Kepler Objects of Interest Table Dataset")
@@ -109,7 +129,9 @@ plt.xticks(rotation=90)
 plt.ylabel("Scaled values")
 plt.tight_layout(pad=0.2)
 
-plt.savefig('../data/figures/boxplot.png')
+plt.savefig('data/figures/boxplot.png')
+
+print("Kastengrafik wurde erstellt.")
 
 
 # Entfernung von Attributen, die Proxies des Zielwertes sind, bzw.  Flag-Werte sind.
@@ -139,7 +161,23 @@ method_names = ("k-Nearest-Neighbours-Klassifikator", "Support-Vector-Machine-Kl
                 "Random-Forest-Klassifikator")
 
 
+# Triviale Hypothese
+
+predict = np.zeros(len(y_test))
+
+cm = confusion_matrix(y_test, predict)
+# tn, fp, fn, tp = cm.ravel()
+
+print("=======================================================================")
+print("TRIVIALE HYPOTHESE")
+print(f"Akkuranz: {accuracy_score(y_test, predict): .04f}")
+print(f"Ca. {(accuracy_score(y_test, predict)*100): .02f}% der Instanzen sind FALSE POSITIVE")
+
 # Gittersuchverfahren für knn-Methode mit höchster Akkuranz
+
+print("=======================================================================")
+print("Suche Kombination von Hyperparametern, welche die Akkuranz maximisieren")
+print(method_names[0])
 
 grid_points_n_neighbors = np.arange(1, 100)
 grid_points_weights = ("uniform", "distance")
@@ -152,7 +190,7 @@ for n_neighbors in grid_points_n_neighbors:
 
         knn = KNeighborsClassifier(
             n_neighbors=n_neighbors, weights=weights, p=1)
-        knn_model_fit = knn.fit(x_train, y_train)
+        knn_model_fit = knn.fit(x_train, y_train.ravel())
         knn_model_predict = knn.predict(x_test)
         z = accuracy_score(knn_model_predict, y_test)
 
@@ -161,9 +199,7 @@ for n_neighbors in grid_points_n_neighbors:
             max_z = z
             best_point_knn = (n_neighbors, weights)
 
-print("=======================================================================")
-print(method_names[0])
-print("Suche Kombination von Hyperparametern, welche die Akkuranz maximisieren")
+
 print(f"Bester Punkt  gefunden bei \n"
       f"n_neighbors = {best_point_knn[0]} \n"
       f"weights = {best_point_knn[1]} \n"
@@ -172,27 +208,21 @@ print(f"Bester Punkt  gefunden bei \n"
 
 # Optimisiertes kNN-Modell
 
+print("Optimiertes kNN-Modell erstellen.")
+
 opt_knn = KNeighborsClassifier(
     n_neighbors=best_point_knn[0], weights=best_point_knn[1], p=1)
-fit_knn = opt_knn.fit(x_train, y_train)
+fit_knn = opt_knn.fit(x_train, y_train.ravel())
 predict_knn = opt_knn.predict(x_test)
 y_proba_knn = opt_knn.predict_proba(x_test)
 accuracy_knn = accuracy_score(y_test, predict_knn)
 cm_knn = confusion_matrix(y_test, predict_knn)
 
-file_to_write = open("../data/models/knn_model.pickle", "wb")
+file_to_write = open("data/models/knn_model.pickle", "wb")
 pickle.dump(opt_knn, file_to_write)
-print("Bestes kNN-Modell gespeichert unter '../data/models/knn_model.pickle'")
+print("Optimiertes kNN-Modell gespeichert unter 'data/models/knn_model.pickle'")
 
-# Triviale Hypothese
 
-predict = np.zeros(len(y_test))
-
-cm = confusion_matrix(y_test, predict)
-# tn, fp, fn, tp = cm.ravel()
-
-print("--------------------------------- \n TRIVIALE HYPOTHESE")
-print(f"Accuracy: {accuracy_score(y_test, predict)}")
 # Gittersuchverfahren für SVC-Methode mit höchster Akkuranz
 
 grid_points_C = np.arange(2, 8, 0.1)
@@ -200,13 +230,17 @@ grid_points_degree = np.arange(5, 8)
 max_z = -np.inf
 best_point_svm = (2, 5)
 
+print("=======================================================================")
+print(method_names[1])
+print("Suche Kombination von Hyperparametern, welche die Akkuranz maximisieren")
+
 for C in grid_points_C:
 
     for degree in grid_points_degree:
 
         svm = SVC(C=C, kernel="poly", degree=degree,
                   gamma="scale", probability=True)
-        svm_model_fit = svm.fit(x_train, y_train)
+        svm_model_fit = svm.fit(x_train, y_train.ravel())
         svm_model_predict = svm.predict(x_test)
         z = accuracy_score(svm_model_predict, y_test)
 
@@ -215,9 +249,7 @@ for C in grid_points_C:
             max_z = z
             best_point_svm = (C, degree)
 
-print("=======================================================================")
-print(method_names[1])
-print("Suche Kombination von Hyperparametern, welche die Akkuranz maximisieren")
+
 print(f"Bester Punkt gefunden bei \n"
       f"C = {best_point_svm[0]} \n"
       f"degree = {best_point_svm[1]} \n"
@@ -226,26 +258,32 @@ print(f"Bester Punkt gefunden bei \n"
 
 # Optimisiertes SVC-Modell
 
+print("Optimiertes SVC-Modell erstellen")
+
 opt_svm = SVC(C=best_point_svm[0], kernel="poly",
               degree=best_point_svm[1], gamma="scale", probability=True)
-fit_svm = opt_svm.fit(x_train, y_train)
+fit_svm = opt_svm.fit(x_train, y_train.ravel())
 predict_svm = opt_svm.predict(x_test)
 y_proba_svm = opt_svm.predict_proba(x_test)
 accuracy_svm = accuracy_score(y_test, predict_svm)
 cm_svm = confusion_matrix(y_test, predict_svm)
 
-file_to_write = open("../data/models/svm_model.pickle", "wb")
+file_to_write = open("data/models/svm_model.pickle", "wb")
 pickle.dump(opt_svm, file_to_write)
-print("Bestes SVC-Modell gespeichert unter '../data/models/svm_model.pickle'")
+print("Optimiertes SVC-Modell gespeichert unter 'data/models/svm_model.pickle'")
 
 
 # Gittersuchverfahren für Zufallswald-Methode mit höchster Akkuranz
 
-grid_points_n_estimators = np.arange(50, 70)
-grid_points_max_depth = np.arange(7, 14)
-grid_points_min_samples_split = np.arange(2, 14)
+grid_points_n_estimators = np.arange(45, 55)
+grid_points_max_depth = np.arange(7, 16)
+grid_points_min_samples_split = np.arange(2, 15)
 max_z = -np.inf
 best_point_rf = (50, 7, 2)
+
+print("=======================================================================")
+print(method_names[2])
+print("Suche Kombination von Hyperparametern, welche die Akkuranz maximisieren")
 
 for n_estimators in grid_points_n_estimators:
 
@@ -254,8 +292,8 @@ for n_estimators in grid_points_n_estimators:
         for min_samples_split in grid_points_min_samples_split:
 
             rf = RandomForestClassifier(n_estimators=n_estimators, criterion="entropy",
-            max_depth=max_depth, min_samples_split=min_samples_split, max_features="auto")
-            rf_fit = rf.fit(x_train, y_train)
+                                        max_depth=max_depth, min_samples_split=min_samples_split, max_features="auto")
+            rf_fit = rf.fit(x_train, y_train.ravel())
             z = accuracy_score(rf.predict(x_test), y_test)
 
             if z > max_z:
@@ -263,9 +301,6 @@ for n_estimators in grid_points_n_estimators:
                 max_z = z
                 best_point_rf = (n_estimators, max_depth, min_samples_split)
 
-print("=======================================================================")
-print(method_names[2])
-print("Suche Kombination von Hyperparametern, welche die Akkuranz maximisieren")
 print(f"Bester Punkt mit Akkuranz ={max_z: .04f} gefunden bei \n"
       f"n_estimators = {best_point_rf[0]} \n"
       f"max_depth = {best_point_rf[1]} \n"
@@ -273,18 +308,21 @@ print(f"Bester Punkt mit Akkuranz ={max_z: .04f} gefunden bei \n"
       f"mit Akkuranz ={max_z: .04f}")
 
 
-# Optimisiertes SVC-Modell
+# Optimisiertes Zufallswald-Modell
+
+print("Optimiertes Zufallswald-Modell erstellen")
 
 opt_rf = RandomForestClassifier(n_estimators=best_point_rf[0], criterion="entropy",
                                 max_depth=best_point_rf[1], min_samples_split=best_point_rf[2],
                                 max_features="auto")
-fit_rf = opt_rf.fit(x_train, y_train)
+fit_rf = opt_rf.fit(x_train, y_train.ravel())
 predict_rf = opt_rf.predict(x_test)
 y_proba_rf = opt_rf.predict_proba(x_test)
 accuracy_rf = accuracy_score(y_test, predict_rf)
 cm_rf = confusion_matrix(y_test, predict_rf)
-file_to_write = open("../data/models/rf_model.pickle", "wb")
+file_to_write = open("data/models/rf_model.pickle", "wb")
 pickle.dump(opt_rf, file_to_write)
+print("Optimiertes Zufallswald-Modell gespeichert unter 'data/models/rf_model.pickle'")
 
 
 # Triviale Hypothese
@@ -312,17 +350,12 @@ print(
 
 # Verwirrungsmatrix der Modelle
 
+print("Verwirrungsmatrizen der Modelle erstellen")
+
 accuracy = (accuracy_knn, accuracy_svm, accuracy_rf)
 cm = [cm_knn, cm_svm, cm_rf]
 method_names = ("k Nearest Neighbours Classifer",
                 "Support Vector Machine Classifier", "Random Forest Classifier")
-print("Erklärung - Confusion Matrix")
-print("FP: FALSE POSITIVE")
-print("CA.: CANDIDATE")
-print("CO.: CONFIRMED")
-print("vorh.: Vorhergesagte Klasse")
-print("tats.: Tatsächliche Klasse")
-print("")
 
 fig = plt.figure(figsize=(20, 5))
 
@@ -336,7 +369,9 @@ for i in range(len(method_names)):
     ax.xaxis.set_ticklabels(['FALSE POSITIVE', 'CANDIDATE', 'CONFIRMED'])
     ax.yaxis.set_ticklabels(['FALSE POSITIVE', 'CANDIDATE', 'CONFIRMED'])
 
-plt.savefig(f'../data/figures/CM.png')
+plt.savefig(f'data/figures/CM.png')
+
+print("Verwirrungsmatrizen der Modelle erstellt unter data/figures/CM.png")
 
 
 # Funktionen für die Erstellung von ROC-Kurven bei mehr als zwei Target-Klassen -
@@ -402,20 +437,22 @@ def plot_roc_curve(tpr, fpr, scatter=True, ax=None):
           fpr: The list of FPRs representing each coordinate.
           scatter: When True, the points used on the calculation will be plotted with the line (default = True).
       '''
-     if ax == None:
-          plt.figure(figsize=(5, 5))
-          ax = plt.axes()
+    if ax == None:
 
-      if scatter:
-          sns.scatterplot(x=fpr, y = tpr, ax = ax)
-      sns.lineplot(x=fpr, y = tpr, ax = ax)
-      sns.lineplot(x=[0, 1], y = [0, 1], color = 'green', ax = ax)
-      plt.xlim(-0.05, 1.05)
-      plt.ylim(-0.05, 1.05)
-      plt.xlabel("False Positive Rate")
-      plt.ylabel("True Positive Rate")
+        plt.figure(figsize=(5, 5))
+        ax = plt.axes()
+
+    if scatter:
+        sns.scatterplot(x=fpr, y=tpr, ax=ax)
+        sns.lineplot(x=fpr, y=tpr, ax=ax)
+        sns.lineplot(x=[0, 1], y=[0, 1], color='green', ax=ax)
+        plt.xlim(-0.05, 1.05)
+        plt.ylim(-0.05, 1.05)
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
 
 
+print("Histogramme und ROC-Kurven der disposition-Werte zueinander erstellen.")
 # Codeblock angepasst von Vinícius Trevisan, "Multiclass classification evaluation with ROC Curves
 # and ROC AUC", Towards Data Science, 12. Februar 2022,
 # https://towardsdatascience.com/multiclass-classification-evaluation-with-roc-curves-and-roc-auc-294fd4617e3a
@@ -431,7 +468,8 @@ for i in range(len(class_list)):
 # Wandle Liste in Array um, passe die Dimensionen so an, dass eine Transponierung der ersten beiden
 # Achsen dazu führt, dass erst drei unterschiedliche Dispositions-Paare im Array stehen und dann die
 # umgekehrten Paare, passe dann wieder Dimensionen an und wandle das Array dann wieder in Liste um.
-class_combos = (np.asarray(class_combos).reshape(3, 2, 2)).transpose(1, 0, 2).reshape(6,2).tolist()
+class_combos = (np.asarray(class_combos).reshape(3, 2, 2)
+                ).transpose(1, 0, 2).reshape(6, 2).tolist()
 
 y_proba_all = np.array([y_proba_knn, y_proba_svm, y_proba_rf])
 ml_method = ["KNN", "SVM", "RF"]
@@ -457,7 +495,8 @@ for i in range(len(ml_method)):
         # Prepares an auxiliary dataframe to help with the plots
         df_aux = x_test.copy()
         df_aux['class'] = y_test
-        df_aux['prob'] = y_proba_all[i, :, c1_index]  # Probability that instances ":" are of class c1
+        # Probability that instances ":" are of class c1
+        df_aux['prob'] = y_proba_all[i, :, c1_index]
 
         # Slices only the subset with both classes
         df_aux = df_aux[(df_aux['class'] == c1_index) |
@@ -467,7 +506,8 @@ for i in range(len(ml_method)):
 
         # Plots the probability distribution for the classes
         ax = plt.subplot(4, 3, j + 1)
-        sns.histplot(x="prob", data=df_aux, hue='class', color='b', ax=ax, bins=bins)
+        sns.histplot(x="prob", data=df_aux, hue='class',
+                     color='b', ax=ax, bins=bins)
 
         ax.set_title(title)
         ax.legend([f"{c1}", f"{c2}"])
@@ -484,25 +524,33 @@ for i in range(len(ml_method)):
         # Calculates the ROC AUC OvO
         roc_auc_ovo[title] = roc_auc_score(df_aux['class'], df_aux['prob'])
 
-    plt.savefig(f'../data/figures/hist_roc_{ml_method[i]}.png')
+    plt.savefig(f'data/figures/hist_roc_{ml_method[i]}.png')
 # Ende angepasster Codeblock
 
     # Anzeige der ROC AUC OvR
-        print(f"ROC AUC von {list(roc_auc_ovo)[j]}: {list(roc_auc_ovo.values())[j]:.4f}")
+    print(
+        f"ROC AUC von {list(roc_auc_ovo)[j]}: {list(roc_auc_ovo.values())[j]:.4f}")
 
     print(f"Mittelwert von ROC AUC: {np.mean(list(roc_auc_ovo.values())):.4f}")
     print("---------------------------------------------------")
 
+print("Histogramme und ROC-Kurven der disposition-Werte zueinander wurden erstellt.")
+
 
 # Permutation Importance und Feature Importance bestimmen und anzeigen
 
-perm_imp_knn = permutation_importance(opt_knn, x_test, y_test, scoring='accuracy')
+print("Feature Importance (nur für Zufallswald) und Permutation Importance bestimmen und plotten.")
+
+perm_imp_knn = permutation_importance(
+    opt_knn, x_test, y_test, scoring='accuracy')
 importance_knn = perm_imp_knn.importances_mean
 
-perm_imp_svm = permutation_importance(opt_svm, x_test, y_test, scoring='accuracy')
+perm_imp_svm = permutation_importance(
+    opt_svm, x_test, y_test, scoring='accuracy')
 importance_svm = perm_imp_svm.importances_mean
 
-perm_imp_rf = permutation_importance(opt_rf, x_test, y_test, scoring='accuracy')
+perm_imp_rf = permutation_importance(
+    opt_rf, x_test, y_test, scoring='accuracy')
 importance_rf = perm_imp_rf.importances_mean
 
 
@@ -525,5 +573,7 @@ plot_one_method(
 plot_one_method(ax[3], "Feature Importance fpr the Random Forest Classifier",
                 opt_rf.feature_importances_)
 plt.tight_layout()
-plt.savefig(f'../data/figures/importances.png')
+plt.savefig(f'data/figures/importances.png')
 fig.tight_layout()
+
+print("Permutation Importances und Feature Importance geplottet: data/figures/importances.png")
